cad995b5e5d4ebc43c25913bdf3509169c65f7de
PraveenWSL <praveenmcaa@gmail.com>
2021-11-03 16:44:35 -0400

    Trying to fix WebVitals.js error

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const reportWebVitals = (onPerfEntry) => {
-=======
-const reportWebVitals = onPerfEntry => {
->>>>>>> d5e8b864cbfc89d4a8195b07cbe6caee1fa1090a
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
