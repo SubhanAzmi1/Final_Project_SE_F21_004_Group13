8be2a1d4441c8b1f2d19c0a7368c4443ec5950d0
PraveenWSL <praveenmcaa@gmail.com>
2021-11-03 17:08:13 -0400

    One error on app.py fixed test

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 Flask app logic for P1M3
 """
@@ -131,33 +130,11 @@ def index():
             "genius_url": genius_url,
         }
     )
-=======
-import flask
-from flask_login.utils import login_required
-import os
-import json
-
-
-app = flask.Flask(__name__, static_folder='./build/static')
-# This tells our Flask app to look at the results of `npm build` instead of the 
-# actual files in /templates when we're looking for the index page file. This allows
-# us to load React code into a webpage. Look up create-react-app for more reading on
-# why this is necessary.
-bp = flask.Blueprint("bp", __name__, template_folder="./build")
-
-@bp.route('/index')
-@login_required
-def index():
-    # TODO: insert the data fetched by your app main page here as a JSON
-    DATA = {"your": "data here"}
-    data = json.dumps(DATA)
->>>>>>> d5e8b864cbfc89d4a8195b07cbe6caee1fa1090a
     return flask.render_template(
         "index.html",
         data=data,
     )
 
-<<<<<<< HEAD
 
 app.register_blueprint(bp)
 
@@ -269,39 +246,3 @@ if __name__ == "__main__":
         host=os.getenv("IP", "0.0.0.0"),
         port=int(os.getenv("PORT", "8081")),
     )
-=======
-app.register_blueprint(bp)
-
-@app.route('/signup')
-def signup():
-	...
-
-@app.route('/signup', methods=["POST"])
-def signup_post():
-	...
-
-@app.route('/login')
-def login():
-    ...
-
-@app.route('/login', methods=["POST"])
-def login_post():
-	...
-
-@app.route('/save', methods=["POST"])
-def save():
-    ...
-
-
-@app.route('/')
-def main():
-	...
-
-
-
-
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8081)),
-)
->>>>>>> d5e8b864cbfc89d4a8195b07cbe6caee1fa1090a
