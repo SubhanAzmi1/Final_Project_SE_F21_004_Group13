8e187c823b5924f60deb2be422a22c051d51f4af
PraveenWSL <praveenmcaa@gmail.com>
2021-11-03 14:43:07 -0400

    Commit of project 1 milestone 3 solution code. Our app can be built upon this

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,43 @@
+<<<<<<< HEAD
+import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import App from './App';
+
+test('renders buttons', () => {
+  render(<App />);
+  const addButton = screen.getByText('Add Artist');
+  expect(addButton).toBeInTheDocument();
+  const saveButton = screen.getByText('Save');
+  expect(saveButton).toBeInTheDocument();
+});
+
+test('add artist', () => {
+  render(<App />);
+  const addButton = screen.getByText('Add Artist');
+  const textInput = screen.getByTestId('text_input');
+  fireEvent.change(textInput, { target: { value: 'Pinegrove' } });
+  fireEvent.click(addButton);
+
+  const artistEntry = screen.getByText('Pinegrove');
+  const deleteButton = screen.getByText('Delete');
+  expect(artistEntry).toBeInTheDocument();
+  expect(deleteButton).toBeInTheDocument();
+});
+
+test('delete artist', () => {
+  render(<App />);
+  const addButton = screen.getByText('Add Artist');
+  const textInput = screen.getByTestId('text_input');
+  fireEvent.change(textInput, { target: { value: 'Pinegrove' } });
+  fireEvent.click(addButton);
+
+  const artistEntry = screen.getByText('Pinegrove');
+  const deleteButton = screen.getByText('Delete');
+  expect(artistEntry).toBeInTheDocument();
+
+  fireEvent.click(deleteButton);
+  expect(artistEntry).not.toBeInTheDocument();
+=======
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -5,4 +45,5 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+>>>>>>> d5e8b864cbfc89d4a8195b07cbe6caee1fa1090a
 });
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
+const reportWebVitals = (onPerfEntry) => {
+=======
 const reportWebVitals = onPerfEntry => {
+>>>>>>> d5e8b864cbfc89d4a8195b07cbe6caee1fa1090a
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
