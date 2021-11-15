933bf69db9d1f380b757ab1c48007da49f6c6eef
cubanmjssle <cpulido1@student.gsu.edu>
2021-11-15 22:57:23 +0000

    added the template that will need little rearranging to be able to randomize the characters and display the results

--- mainpage.py
+++ mainpage.py
@@ -1,11 +1,14 @@
-import marvel as Marvel
-  
-m = Marvel("f4d3befbd6c697e537cafcf9953ccbcf",
-           "f93a2caa1a2831e29d209f2ab90294787fde7fcc")
+from marvel import Marvel
+import json
+#endpoint = "http(s)://gateway.marvel.com/"
+
+PUBLIC_KEY = 'f4d3befbd6c697e537cafcf9953ccbcf'
+PRIVATE_KEY = 'f93a2caa1a2831e29d209f2ab90294787fde7fcc'
+m = Marvel(PUBLIC_KEY, PRIVATE_KEY)
   
 # getting the characters object
 characters = m.characters 
-  
+all_characters=characters.all()
 # serial code of your favourite character
 # this can be different according to your preference
 x = 1011334 
@@ -16,6 +19,7 @@ for n in range (0, 6):
       
     x = x+1 
     for i in range (1,12):
-      print(all_characters['data']['results'][int(i)]['title'])
+      print(all_characters)
+      #['data']['results'][int(i)]['title'])
 
 
