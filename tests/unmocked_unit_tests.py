6aebbc22b35587dcd314b1b95a8ea7e78d104d04
SubhanAzmi1 <smazmi7@gmail.com>
2021-11-16 21:47:52 -0500

    finished unmocked tests.

--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -14,30 +14,21 @@ parent = os.path.dirname(current)
 # the sys.path.
 sys.path.append(parent)
 
-from spotify import extract_song_data, get_combined_song_artists_string
 from marvel import get_comic_data
 
 INPUT = "INPUT"
 EXPECTED_OUTPUT = "EXPECTED_OUTPUT"
 
 
-
 class GetComicDataTests(unittest.TestCase):
-	"""
-	For testing Marvel search
-	"""
-
-    # no api key will always return empty arrays....
-    # since api key is from environ...
     def test_get_comic_data_1(self):
         self.assertEqual(get_comic_data("", "other"), ([], [], [], [], []))
-		
-	def test_get_comic_data_2(self):
-        self.assertNotEqual(get_comic_data("Zzxxx", "zz"), ([], [], [], [], []))
 
-
+    def test_get_comic_data_2(self):
+        self.assertEqual(
+            get_comic_data("Iron Man (2020) #13", "exact"), ([], [], [], [], [])
+        )
 
 
 if __name__ == "__main__":
     unittest.main()
-	
