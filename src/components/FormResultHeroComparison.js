0204bf23dbd562ec13473a89a8550578b1f7365a
SubhanAzmi1 <smazmi7@gmail.com>
2021-12-02 00:52:39 -0500

    made progress on displayresult for crossover search. wondering if its better to send one string instead of a list for common items.

--- src/components/FormResultHeroComparison.js
+++ src/components/FormResultHeroComparison.js
@@ -6,53 +6,89 @@ import React from 'react';
 // This component appears with the results given from an API.
 // sources:
 // tables https://www.youtube.com/watch?v=hson9BXU9F8
-// iterate thorugh json: https://stackoverflow.com/questions/43241139/looping-through-table-data-react-js
+// put images on either side:
+//  https://stackoverflow.com/questions/16603884/align-2-images-one-to-right-other-to-left-inside-div
 // html tables: https://www.w3schools.com/html/html_tables.asp
+
 
 function FormResultHeroComparison({
   noResults, nameList2, images2, comicsCommon, storiesCommon, eventsCommon,
 }) {
+  let leftName = null;
+  let leftImage = null;
+  let rightName = null;
+  let rightImage = null;
+  const someIndex0 = 0;
+  const someIndex1 = 0;
+  if (nameList2.length > 1) {
+    if (nameList2[someIndex0] === 'wd') {
+      leftName = 'Who is dat?';
+      leftImage = '../images/hulk-thinking.jpg';
+    } else {
+      leftName = nameList2[someIndex0];
+      leftImage = images2[someIndex0];
+    }
+    if (nameList2[someIndex1] === 'wd') {
+      rightName = 'Who is dat?';
+      rightImage = '../images/hulk-thinking.jpg';
+    } else {
+      rightName = nameList2[someIndex1];
+      rightImage = images2[someIndex1];
+    }
+  }
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            Common
-          </tr>
-        </thead>
-      </table>
-      {isHero === true ? (
-        <table border="1" cellPadding="0.25" id="displayfavtable">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Date_modified</th>
-              <th>Description</th>
-              <th>ID</th>
-              <th>Add to Favorites!</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData}
-          </tbody>
-        </table>
+      <div id="hero_COS_image_outer">
+        <div id="left_hero">
+          <img
+            src={leftImage}
+            width={150} // to be determined
+            alt="Hero_1 or confused_hulk"
+          />
+          {leftName}
+        </div>
+        <div id="right_hero">
+          <img
+            src={rightImage}
+            width={150} // to be determined
+            alt="Hero_2 or confused_hulk"
+          />
+          {rightName}
+        </div>
+      </div>
+      {noResults ? (
+        <div>
+          <img
+            src="../images/comic-hulk-angry.jpg"
+            width={300} // to be determined
+            alt="Angry hulk"
+          />
+          ArGh! Nothin in commn.
+        </div>
       ) : (
-        <table border="1" cellPadding="0.25" id="displayfavtable">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Title</th>
-              <th>Date_published</th>
-              <th>Series</th>
-              <th>ID</th>
-              <th>Add to Favorites!</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData}
-          </tbody>
-        </table>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                Common
+              </tr>
+            </thead>
+            <thead>
+              <tr>
+                Comics
+              </tr>
+              <tr>
+                Stories
+              </tr>
+              <tr>
+                Events
+              </tr>
+            </thead>
+            <tbody>
+              {tableData}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
